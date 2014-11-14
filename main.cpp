d7bc72b872f086cc8c09bd0fd4b85f94d194f203
Oliver Williams <ollie@olliewilliams.org>
2014-11-14 14:51:36 -0800

    Adding event handlers.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,36 @@
 #define __MACOS__
 #include "EDSDK/Header/EDSDK.h"
 
+EdsError EDSCALLBACK handleObjectEvent(
+    EdsObjectEvent event,
+    EdsBaseRef     object,
+    EdsVoid*       context  )
+{
+  std::cout << "Object event.\n";
+  return EDS_ERR_OK;
+}
+
+
+EdsError EDSCALLBACK handlePropertyEvent(
+    EdsPropertyEvent        inEvent,
+    EdsPropertyID           inPropertyID,
+    EdsUInt32               inParam,
+    EdsVoid *               inContext )
+{
+  std::cout << "Property event.\n";
+  return EDS_ERR_OK;
+}
+
+EdsError EDSCALLBACK handleStateEvent(
+    EdsStateEvent           inEvent,
+    EdsUInt32               inEventData,
+    EdsVoid *               inContext )
+{
+  std::cout << "State event.\n";
+  return EDS_ERR_OK;
+}
+
+
 int main()
 {
   EdsError err;
@@ -29,6 +59,40 @@ int main()
   }
   std::cout << "There are " << cameraCount << " cameras connected.\n";
 
+  if (cameraCount > 0) {
+
+    EdsCameraRef camera = nullptr;
+    err = EdsGetChildAtIndex(cameraList, 0, &camera);
+    if (EDS_ERR_OK != err)
+    {
+      throw err;
+    }
+
+    // Set event handler
+    err = EdsSetObjectEventHandler(camera, kEdsObjectEvent_All, handleObjectEvent, NULL);
+    if (EDS_ERR_OK != err)
+    {
+      throw err;
+    }
+    
+    // Set event handler
+    err = EdsSetPropertyEventHandler(camera, kEdsPropertyEvent_All, handlePropertyEvent, NULL);
+    if (EDS_ERR_OK != err)
+    {
+      throw err;
+    }
+    
+    // Set event handler
+    err = EdsSetCameraStateEventHandler(camera, kEdsStateEvent_All, handleStateEvent, NULL);
+    if (EDS_ERR_OK != err)
+    {
+      throw err;
+    }
+
+    
+    EdsRelease(camera);    
+  }
+
   EdsRelease(cameraList);
 
   std::cout << "Unitializing library\n";
@@ -36,3 +100,10 @@ int main()
   
   return 0;
 }
+
+
+
+
+
+
+
