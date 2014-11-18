2e5775af9272d63573a89f0539645841099bb902
Oliver Williams <ollie@olliewilliams.org>
2014-11-17 16:31:27 -0800

    Removing old live view stuff.

--- main.cpp
+++ main.cpp
@@ -4,53 +4,6 @@
 
 using namespace dslr;
 
-#if 0
-EdsError GetCurrentDevice(EdsCameraRef camera, EdsUInt32* device)
-{
-  // Get current device setting
-  return EdsGetPropertyData(camera,
-                           kEdsPropID_Evf_OutputDevice,
-                           0,
-                           sizeof(*device),
-                           device);
-}
-
-EdsError SetDevice(EdsCameraRef camera, EdsUInt32 device)
-{
-  return SetProperty<kEdsPropID_Evf_OutputDevice>(camera, device);
-}
-
-EdsError StartLiveView(EdsCameraRef camera)
-{
-#if 0
-  EdsError err;  
-  EdsUInt32 device;
-  err = GetCurrentDevice(camera, &device);
-  if (EDS_ERR_OK != err) {
-    return err;
-  }
-  std::cout << "Device = " << device << std::endl;
-  device |= kEdsEvfOutputDevice_PC;
-#endif 
-  return SetDevice(camera, kEdsEvfOutputDevice_PC);
-}
-
-EdsError EndLiveView(EdsCameraRef camera)
-{
-#if 0
-  EdsError err;  
-  EdsUInt32 device;
-  err = GetCurrentDevice(camera, &device);
-  if (EDS_ERR_OK != err) {
-    return err;
-  }
-
-  device &= ~kEdsEvfOutputDevice_TFT;
-#endif
-  return SetDevice(camera, 0);  
-}
-#endif
-
 int main()
 {
   EdsError err;
